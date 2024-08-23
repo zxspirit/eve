5ab57941a3e90655d0b4445b1f08499de7dac2a3
zhxu <zheng2580369@gmail.com>
2024-08-23 12:48:31 +0800

    开发

---
+++ ddns/cloudflare.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+func main() {
+	// Construct a new API object using a global API key
+	getenv := os.Getenv("CLOUDFLARE_API_KEY")
+	fmt.Println(getenv)
+	api, err := cloudflare.New(getenv, os.Getenv("CLOUDFLARE_API_EMAIL"))
+	// alternatively, you can use a scoped API token
+	// api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Most API calls require a Context
+	ctx := context.Background()
+
+	// Fetch user details on the account
+	u, err := api.UserDetails(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print user details
+	fmt.Println(u)
+}
